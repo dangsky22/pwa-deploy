0d27adbd515cebbeed21191544f19b67c6d91e69
dangrizky <rizkylossaga3@gmail.com>
2025-08-21 14:45:05 +0700

    hadeh

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('landing.dashboard'); // root diarahkan ke dashboard
+    return view('dashboard'); // root diarahkan ke dashboard
 })->name('home');
 
 Route::get('/dashboard', function () {
-    return view('landing.dashboard'); // tambahin route dashboard
+    return view('dashboard'); // route dashboard
 })->name('dashboard');
 
 Route::get('/offline', function () {
-    return view('offline');
+    return view('offline'); // halaman offline
 })->name('offline');
