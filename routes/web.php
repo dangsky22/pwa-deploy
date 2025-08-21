89de9bf0082f090f9f6ba1097d5536fb9918ee41
dangrizky <rizkylossaga3@gmail.com>
2025-08-21 15:08:08 +0700

    baru

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController; // Pastikan controller ini ada
 
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
 Route::get('/', function () {
-    return view('dashboard'); // root diarahkan ke dashboard
+    return view('landing.dashboard'); // root diarahkan ke dashboard
 })->name('home');
 
 Route::get('/dashboard', function () {
