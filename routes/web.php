132514814c444c94fac9db4048932e2c26a5c92c
dangrizky <rizkylossaga3@gmail.com>
2025-08-21 13:55:49 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('landing.dashboard'); // ganti ke dashboard
+    return view('landing.dashboard'); // root diarahkan ke dashboard
+})->name('home');
+
+Route::get('/dashboard', function () {
+    return view('landing.dashboard'); // tambahin route dashboard
 })->name('dashboard');
 
 Route::get('/offline', function () {
