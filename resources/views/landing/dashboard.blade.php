656af0d507f7621794fc48c4ea026fbad8b9eef0
dangrizky <rizkylossaga3@gmail.com>
2025-08-22 13:41:40 +0700

    cobain

--- resources/views/landing/dashboard.blade.php
+++ resources/views/landing/dashboard.blade.php
@@ -211,8 +211,7 @@
             <div class="absolute inset-0 w-full h-full bg-cover bg-center bg-no-repeat" 
                  style="background-image: url('{{ asset('images/icons/kost.png') }}');">
             </div>
-            <!-- Light overlay for better text readability -->
-            <div class="absolute inset-0 bg-black bg-opacity-30"></div>
+            
         </div>
 
         <!-- Content -->
