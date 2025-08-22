9f672a0c67103746e94dadd174f7eb826519b450
dangrizky <rizkylossaga3@gmail.com>
2025-08-22 08:22:22 +0700

    baru

--- resources/views/landing/dashboard.blade.php
+++ resources/views/landing/dashboard.blade.php
@@ -1,10 +1,9 @@
-{{-- resources/views/landing/dashboard.blade.php --}}
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- PWA  -->
+    <!-- PWA -->
     <meta name="theme-color" content="#6777ef"/>
     <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
@@ -174,20 +173,18 @@
 
     <script src="{{ asset('/sw.js') }}"></script>
     <script>
-   if ("serviceWorker" in navigator) {
-      // Register a service worker hosted at the root of the
-      // site using the default scope.
-      navigator.serviceWorker.register("/sw.js").then(
-      (registration) => {
-         console.log("Service worker registration succeeded:", registration);
-      },
-      (error) => {
-         console.error(`Service worker registration failed: ${error}`);
-      },
-    );
-      } else {
-     console.error("Service workers are not supported.");
-      }
+        if ("serviceWorker" in navigator) {
+            navigator.serviceWorker.register("/sw.js").then(
+                (registration) => {
+                    console.log("Service worker registration succeeded:", registration);
+                },
+                (error) => {
+                    console.error(`Service worker registration failed: ${error}`);
+                },
+            );
+        } else {
+            console.error("Service workers are not supported.");
+        }
     </script>
 </body>
 </html>
